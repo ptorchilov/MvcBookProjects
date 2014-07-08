b767c7ccd14560a4c2e1809c1e93bc5fc49854c6
p.torchilov <p.torchilov@gmail.com>
2014-07-08 15:33:26 +0300

    Made first actions.

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace PartyInvites.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        
+        public ViewResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyInvites.Models
+{
+    using System;
+
+    public class GuestResponse
+    {
+        public String Name { get; set; }
+
+        public String Email { get; set; }
+
+        public String Phone { get; set; }
+
+        public bool? WillAttend { get; set; }
+    }
+}
