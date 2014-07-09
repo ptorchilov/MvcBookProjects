6b8354254784ea3dc0f42bb933137b2f731c10c3
p.torchilov <p.torchilov@gmail.com>
2014-07-09 13:12:17 +0300

    Add auto properties.

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿namespace LanguageFeatures.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    using LanguageFeatures.Models;
+
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public String Index()
+        {
+            return "Navigate to URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            var product = new Product();
+
+            product.Name = "Kobyak";
+
+            var productName = product.Name;
+
+            return View("Result", (Object) String.Format("Product name: {0}", productName));
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace LanguageFeatures.Models
+{
+    using System;
+
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public String Name { get; set; }
+        p--
+        public String Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public String Catigory { get; set; }
+    }
+}
