df226c3bef5b7611cb3a0f0497165ebc0166bdd4
p.torchilov <p.torchilov@gmail.com>
2014-07-09 13:12:45 +0300

    Correct error.

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,7 +7,7 @@
         public int ProductID { get; set; }
 
         public String Name { get; set; }
-        p--
+        
         public String Description { get; set; }
 
         public decimal Price { get; set; }
