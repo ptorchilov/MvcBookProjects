546526e72ac92775547804c304582be214f4e1d4
p.torchilov <p.torchilov@gmail.com>
2014-07-09 13:55:22 +0300

    Add object initializers.

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿namespace LanguageFeatures.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Web.Mvc;
 
     using LanguageFeatures.Models;
@@ -10,20 +11,50 @@
         //
         // GET: /Home/
 
+        #region Public Methods and Operators
+
+        public ViewResult AutoProperty()
+        {
+            var product = new Product { Name = "Kobyak" };
+
+            string productName = product.Name;
+
+            return View("Result", (Object)String.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            String[] stringArray = { "apple", "orange", "plum" };
+
+            var intList = new List<int> { 10, 20, 30, 40 };
+
+            var myDictionary = new Dictionary<String, int>
+                                   {
+                                       { "apple", 10 }, { "orange", 20 }, { "plum", 30 }
+                                   };
+
+            return View("Result", (Object) stringArray[1]);
+        }
+
+        public ViewResult CreateProduct()
+        {
+            var myProduct = new Product
+                                {
+                                    ProductID = 100,
+                                    Name = "Kobyak",
+                                    Description = "A boat for one person",
+                                    Price = 270,
+                                    Catigory = "Watersports"
+                                };
+
+            return View("Result", (Object)String.Format("Category: {0}", myProduct.Price));
+        }
+
         public String Index()
         {
             return "Navigate to URL to show an example";
         }
 
-        public ViewResult AutoProperty()
-        {
-            var product = new Product();
-
-            product.Name = "Kobyak";
-
-            var productName = product.Name;
-
-            return View("Result", (Object) String.Format("Product name: {0}", productName));
-        }
+        #endregion
     }
 }
