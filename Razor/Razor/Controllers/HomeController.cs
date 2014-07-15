76be09ba6910c978f04fe66758d788084f00eeca
p.torchilov <p.torchilov@gmail.com>
2014-07-15 17:18:48 +0300

    Add array model for Razor.

--- Razor/Razor/Controllers/HomeController.cs
+++ Razor/Razor/Controllers/HomeController.cs
@@ -37,5 +37,18 @@
 
             return View(myProduct);
         }
+
+        public ActionResult DemoArray()
+        {
+            var array = new []
+                {
+                    new Product { Name = "Kobyak", Price = 275M },
+                    new Product { Name = "Lifejacker", Price = 48.95M},
+                    new Product { Name = "Soccer ball", Price = 19.50M},
+                    new Product { Name = "Corner flag", Price = 34.95M}
+                };
+
+            return View(array);
+        }
     }
 }
