4fe4344a061c00255754f2f1f2998a699ad42df2
p.torchilov <p.torchilov@gmail.com>
2014-07-17 11:59:53 +0300

    Add models.

---
+++ EssentialTools/EssentialTools/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EssentialTools.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public String Category { get; set; }
+    }
+}
---
+++ EssentialTools/EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private readonly LinqValueCalculator calculator;
+
+        public ShoppingCart(LinqValueCalculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return calculator.ValueProducts(Products);
+        }
+    }
+}
