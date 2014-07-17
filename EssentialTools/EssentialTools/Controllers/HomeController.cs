d29ccbba59c0b749579721738efa367d4d75cfc8
p.torchilov <p.torchilov@gmail.com>
2014-07-17 12:04:39 +0300

    Add controller.

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿namespace EssentialTools.Controllers
+{
+    using System.Web.Mvc;
+    using EssentialTools.Models;
+
+    public class HomeController : Controller
+    {
+        private readonly Product[] products =
+            {
+                new Product { Name = "Kobyak", Category = "Watersports", Price = 275M },
+                new Product { Name = "Lifejacker", Category = "Watersports", Price = 48.95M },
+                new Product { Name = "Soccer ball", Category = "Soccer", Price = 19.50M },
+                new Product { Name = "Corner flag", Category = "Soccer", Price = 34.95M }
+            };
+
+        public ActionResult Index()
+        {
+            var calculator = new LinqValueCalculator();
+            var cart = new ShoppingCart(calculator)
+                           {
+                               Products = products
+                           };
+
+            var totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+
+    }
+}
