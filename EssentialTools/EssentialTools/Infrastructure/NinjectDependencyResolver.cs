2e5848fc8d36c569ee9fe8d16a78795747db86a0
p.torchilov <p.torchilov@gmail.com>
2014-08-12 23:42:38 +0300

    Add Dependency Resolver class.

---
+++ EssentialTools/EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,38 @@
+﻿namespace EssentialTools.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using EssentialTools.Models;
+
+    using Ninject;
+
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver()
+        {
+            kernel = new StandardKernel();
+            AddBindings();
+        }
+
+        public Object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<Object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+        }
+    }
+}
