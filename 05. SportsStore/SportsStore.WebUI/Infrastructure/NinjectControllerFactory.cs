a5d7d88fcb915dd4b04289ed8a4ac48df7387371
Pavel Torchilov <p.torchilov@itransition.com>
2014-08-13 17:48:48 +0300

    Add Sports Store project and DI factory.

---
+++ 05. SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace SportsStore.WebUI.Infrastructure
+{
+    using System;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Ninject;
+
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController) ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            
+        }
+    }
+}
