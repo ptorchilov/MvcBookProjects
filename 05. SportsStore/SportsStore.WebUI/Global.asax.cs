f0a317ea16b734819d93f13a1f7ea572fb9b6a24
Pavel Torchilov <p.torchilov@itransition.com>
2014-08-13 17:51:01 +0300

    Add DI factory initialization.

--- 05. SportsStore/SportsStore.WebUI/Global.asax.cs
+++ 05. SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace SportsStore.WebUI
+﻿namespace SportsStore.WebUI
 {
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using Infrastructure;
+
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -22,6 +20,8 @@ namespace SportsStore.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
         }
     }
 }
