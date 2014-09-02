ab392afb1c42ca1d9bccfd5d63bf884d571c71c6
Pavel Torchilov <p.torchilov@itransition.com>
2014-09-02 14:09:23 +0300

    Add pagination for products.

--- 05. SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ 05. SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -2,19 +2,25 @@
 {
     using System.Web.Mvc;
     using Domain.Abstract;
+    using System.Linq;
 
     public class ProductController : Controller
     {
         private IProductRepository repository;
+
+        public int PageSize = 4;
 
         public ProductController(IProductRepository productRepository)
         {
             repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
